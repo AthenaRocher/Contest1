10959941d1b18797998d7cee7e22d3823c45483d
Maurakshi Banerjee <104708058+AthenaRocher@users.noreply.github.com>
2022-09-28 01:16:32 +0530

    Update 9.js

--- 9.js
+++ 9.js
@@ -1,25 +1,16 @@
-/*For-Loop for Javascript can be described from it's syntax as follows---
+/* The JavaScript for of statement loops through the values of an iterable object.
 
-for (initialExpression; condition; updateExpression) 
-{
-    // for loop body
+It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more
+ Syntax : 
+for (variable of iterable) {
+  // code block to be executed
 }
 
-here, 
-1. The initialExpression initializes and/or declares variables and executes only once.
-2. The condition is evaluated.
-   * If the condition is false, the for loop is terminated.
-   * If the condition is true, the block of code inside of the for loop is executed.
-3. The updateExpression updates the value of initialExpression when the condition is true.
-4. The condition is evaluated again. This process continues until the condition is false.
-
-The following program shall act as an example:
+The example below is of à for looping over Array :
 */
-//Writing a program to Display Numbers from 1 to 5--------Here we will be using for loop to avoid the unnessasry usage of program lines.
+const cars = ["BMW", "Volvo", "Mini"];
 
-const n = 5;
-for (let i = 1; i <= n; i++) {
-    console.log(i); 
+let text = "";
+for (let x of cars) {
+  text += x;
 }
-
-// in line 21 we witness the looping from i = 1 to 5 and in each iteration, i is increased by 1 
