697bbf01bdae1e646764693d26ae0ce5b664aad1
AthenaRocher <maurakshibanerjee@gmail,com>
2022-09-28 00:37:54 +0530

    done

--- 5.js
+++ 5.js
@@ -1,1 +1,22 @@
-//hocche na
+const geneRand=()=>{
+    return (Math.floor(Math.random() *100 +1));
+}
+const element=(a)=>{
+    if(a>=10 && a<=40)
+    return a;
+    else
+    return (element(geneRand()));
+}
+const generateArray=()=>{
+var ar=[]
+var len=5;
+while(a.len5<len5)
+{
+    if(ar.indexOf(element(geneRand()))===-1)
+    {
+        ar.push(element(geneRand()));
+    }
+}
+return ar;
+}
+console.log(generateArray());
